366dd54183bc86a2bb9aaddf922a09caa2204868
hfungwi <Harrisfungwi14@gmail.com>
2026-01-20 14:57:19 -0600

    Create leetcode_db_608.sql

---
+++ leetcode_db_608.sql
@@ -0,0 +1,103 @@
+-- please scroll to bottom for solution
+-- question is presented "as is" from leetcode. 
+-- Question difficulty on leetcode is marked as "medium"
+-- Solution was developed indepently by myself
+-- link to problem is : https://leetcode.com/problems/tree-node/
+
+--Start of problem ;
+/*
+Table: Tree
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| p_id        | int  |
++-------------+------+
+id is the column with unique values for this table.
+Each row of this table contains information about the id of a node and the id of its parent node in a tree.
+The given structure is always a valid tree.
+ 
+
+Each node in the tree can be one of three types:
+
+"Leaf": if the node is a leaf node.
+"Root": if the node is the root of the tree.
+"Inner": If the node is neither a leaf node nor a root node.
+Write a solution to report the type of each node in the tree.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+
+Input: 
+Tree table:
++----+------+
+| id | p_id |
++----+------+
+| 1  | null |
+| 2  | 1    |
+| 3  | 1    |
+| 4  | 2    |
+| 5  | 2    |
++----+------+
+Output: 
++----+-------+
+| id | type  |
++----+-------+
+| 1  | Root  |
+| 2  | Inner |
+| 3  | Leaf  |
+| 4  | Leaf  |
+| 5  | Leaf  |
++----+-------+
+Explanation: 
+Node 1 is the root node because its parent node is null and it has child nodes 2 and 3.
+Node 2 is an inner node because it has parent node 1 and child node 4 and 5.
+Nodes 3, 4, and 5 are leaf nodes because they have parent nodes and they do not have child nodes.
+Example 2:
+
+
+Input: 
+Tree table:
++----+------+
+| id | p_id |
++----+------+
+| 1  | null |
++----+------+
+Output: 
++----+-------+
+| id | type  |
++----+-------+
+| 1  | Root  |
++----+-------+
+Explanation: If there is only one node on the tree, you only need to output its root attributes.
+ 
+
+Note: This question is the same as 3054: Binary Tree Nodes.
+*/
+--end of problem;
+--
+--
+--Start of Solution
+SELECT 
+     id,
+     CASE 
+     when p_id is null      and p_level = 1 then 'Root'
+     when p_id is not null  and id in (select distinct p_id from tree) then 'Inner'
+     when p_id is not null  and id not in (select distinct p_id from tree)  then 'Leaf'
+     ELSE 'Leaf'
+     END as type
+from
+(select distinct id, P_id, level p_level
+from tree
+start with p_id is null
+connect by prior id = p_id
+order by id
+)
+--End of Solution
