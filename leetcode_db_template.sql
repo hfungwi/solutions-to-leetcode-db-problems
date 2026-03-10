c32192694e9ecf638955ed99ebb30adfb034d014
hfungwi <Harrisfungwi14@gmail.com>
2026-03-10 10:56:51 -0500

    Create leetcode_db_template.sql

---
+++ leetcode_db_template.sql
@@ -0,0 +1,17 @@
+-- please scroll to bottom for solution
+-- question is presented "as is" from leetcode. 
+-- Question difficulty on leetcode is marked as " "
+-- Solution was developed indepently by myself
+-- link to problem is : 
+
+--Start of problem ;
+/*
+
+*/
+--end of problem;
+--
+--
+--Start of Solution
+/* Write your PL/SQL query statement below */
+
+--End of Solution
